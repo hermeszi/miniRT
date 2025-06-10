96a5b27c15575b430d57ca649037f94c58d652da
mingde <myuen@student.42singapore.sg>
2025-06-10 13:22:00 +0800

    refactor free display

--- src/ft_free_all.c
+++ src/ft_free_all.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_free_all.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jngew <jngew@student.42singapore.sg>       +#+  +:+       +#+        */
+/*   By: myuen <myuen@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/09 16:21:07 by myuen             #+#    #+#             */
-/*   Updated: 2025/06/10 11:25:37 by jngew            ###   ########.fr       */
+/*   Updated: 2025/06/10 13:18:10 by myuen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,26 @@
 void	free_scene(t_scene *scene)
 {
 	if (!scene)
-		return;
+		return ;
 	if (scene->objects)
 		free(scene->objects);
 	free(scene);
+}
+
+void	free_display(t_display *display)
+{
+	if (!display)
+		return ;
+	if (display->img_ptr)
+		mlx_destroy_image(display->mlx_ptr, display->img_ptr);
+	if (display->win_ptr)
+		mlx_destroy_window(display->mlx_ptr, display->win_ptr);
+	if (display->mlx_ptr)
+	{
+		mlx_destroy_display(display->mlx_ptr);
+		free(display->mlx_ptr);
+	}
+	free(display);
 }
 
 void	free_all(t_main *app)
@@ -26,17 +42,5 @@ void	free_all(t_main *app)
 	if (app->scene)
 		free_scene(app->scene);
 	if (app->display)
-	{
-		if (app->display->img_ptr)
-			mlx_destroy_image(app->display->mlx_ptr, app->display->img_ptr);
-		if (app->display->win_ptr)
-			mlx_destroy_window(app->display->mlx_ptr, app->display->win_ptr);
-		if (app->display->mlx_ptr)
-		{
-			mlx_destroy_display(app->display->mlx_ptr); // <-- this one is crucial
-			free(app->display->mlx_ptr);
-		}
-		free(app->display);
-	}
-
+		free_display(app->display);
 }
