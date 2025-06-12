ac4d535bff2ccb8d180635f08ef1f5b9baeca305
MaxNgew <maxngewjs@gmail.com>
2025-06-12 09:36:18 +0800

    added 2 mroe vec3 functions

--- ft_minirt.h
+++ ft_minirt.h
@@ -264,5 +264,7 @@ t_vec3		vec3_multiply(t_vec3 v, double scalar);
 t_vec3		vec3_cross(t_vec3 v1, t_vec3 v2);
 t_vec3		vec3_new(double x, double y, double z);
 double		vec3_dot(t_vec3 v1, t_vec3 v2);
+t_vec3		vec3_mult_vec3(t_vec3 v1, t_vec3 v2);
+t_color		color_mult(t_color c1, t_color c2);
 
 #endif
