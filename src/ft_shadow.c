7aa6d5f28ad6ab8a25dbb1da35ab4b6aecf95788
mingde <myuen@student.42singapore.sg>
2025-06-28 16:06:21 +0800

    edit test scenes

--- src/ft_shadow.c
+++ src/ft_shadow.c
@@ -6,7 +6,7 @@
 /*   By: myuen <myuen@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 10:11:34 by jngew             #+#    #+#             */
-/*   Updated: 2025/06/28 14:49:34 by myuen            ###   ########.fr       */
+/*   Updated: 2025/06/28 15:54:21 by myuen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,13 @@ static t_color	calculate_ambient_light(t_color object_color, t_ambient ambient)
 t_color	calculate_shading(t_hit *hit, t_scene *scene, int in_shadow)
 {
 	t_color	final_color;
-	t_color	obj_color;
+	t_color	temp_color;
 	
 	final_color = calculate_ambient_light(hit->color, scene->ambient);
 	if (!in_shadow)
 	{
-		obj_color =  calculate_point_light(hit, scene);
-		final_color = color_add(final_color,obj_color);
+		temp_color =  calculate_point_light(hit, scene);
+		final_color = color_add(final_color,temp_color);
 	}
 	return (final_color);
 }
